85b27c90dc3c414b3316151e3e866191a4575d6a
magicarm22 <magicarm22@rambler.ru>
2019-09-28 11:16:25 +0300

    [FIX] Change size in editro

--- Basic training/wolf3D/sources/editor/change_map_size.c
+++ Basic training/wolf3D/sources/editor/change_map_size.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   change_map_size.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eharrag- <eharrag-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: djast <djast@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/26 10:12:18 by eharrag-          #+#    #+#             */
-/*   Updated: 2019/09/26 11:09:13 by eharrag-         ###   ########.fr       */
+/*   Updated: 2019/09/28 10:50:15 by djast            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ static int		check_size_y(t_sdl *sdl)
 			free_map(sdl->mesh);
 			sdl->mesh->size_y -= 1;
 			init_map(&(sdl->mesh));
+			return (1);
 		}
 	}
 	return (0);
@@ -41,6 +42,7 @@ static int		check_size_y_int_max(t_sdl *sdl)
 			free_map(sdl->mesh);
 			sdl->mesh->size_y += 1;
 			init_map(&(sdl->mesh));
+			return (1);
 		}
 	}
 	return (0);
@@ -58,6 +60,7 @@ static int		check_size_x(t_sdl *sdl)
 			free_map(sdl->mesh);
 			sdl->mesh->size_x -= 1;
 			init_map(&(sdl->mesh));
+			return (1);
 		}
 	}
 	return (0);
@@ -75,6 +78,7 @@ static int		check_size_x_int_max(t_sdl *sdl)
 			free_map(sdl->mesh);
 			sdl->mesh->size_x += 1;
 			init_map(&(sdl->mesh));
+			return (1);
 		}
 	}
 	return (0);
@@ -82,13 +86,13 @@ static int		check_size_x_int_max(t_sdl *sdl)
 
 int				change_map_size(t_sdl *sdl)
 {
-	if (check_size_x_int_max(sdl) == 0)
+	if (check_size_x_int_max(sdl) == 1)
 		return (1);
-	else if (check_size_x(sdl) == 0)
+	else if (check_size_x(sdl) == 1)
 		return (1);
-	if (check_size_y_int_max(sdl) == 0)
+	if (check_size_y_int_max(sdl) == 1)
 		return (1);
-	else if (check_size_y(sdl) == 0)
+	else if (check_size_y(sdl) == 1)
 		return (1);
 	return (0);
 }
